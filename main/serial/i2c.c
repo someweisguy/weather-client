19a4d8629e0ce3c5720ab61049c627d175807ca8
Mitch Weisbrod <mitch@theweisbrods.com>
2020-09-15 19:04:31 -0700

    refactor and reduce

--- main/serial/i2c.c
+++ main/serial/i2c.c
@@ -1,229 +1,69 @@
 #include "i2c.h"
-static const char *TAG = "i2c";
-SemaphoreHandle_t i2c_semaphore;
 
-bool i2c_start() {
-	// Configure the i2c port
-	ESP_LOGV(TAG, "Configuring the I2C port");
-	i2c_config_t i2c_config;
-	i2c_config.mode = I2C_MODE_MASTER;
-	i2c_config.master.clk_speed = 100000;
-	i2c_config.sda_io_num = PIN_NUM_SDA;
-	i2c_config.scl_io_num = PIN_NUM_SCL;
-	i2c_config.sda_pullup_en = true;
-	i2c_config.scl_pullup_en = true;
-	esp_err_t config_ret = i2c_param_config(I2C_PORT, &i2c_config);
-	if (config_ret != ESP_OK) {
-		ESP_LOGE(TAG, "Unable to configure the I2C port (%i)", config_ret);
-		return false;
-	}
+#define I2C_PORT I2C_NUM_1
+#define PIN_NUM_SDA 23 // Adafruit Feather 32 Default
+#define PIN_NUM_SCL 22 // Adafruit Feather 32 Default
 
-	// Install the driver
-	ESP_LOGD(TAG, "Installing the I2C port driver");
-	esp_err_t install_ret = i2c_driver_install(I2C_PORT, i2c_config.mode, 0,
-			0, 0);
-	if (install_ret != ESP_OK) {
-		ESP_LOGE(TAG, "Unable to install the I2C port (%i)", install_ret);
-		return false;
-	}
-
-	// Create the i2c semaphore and unlock the thread
-	i2c_semaphore = xSemaphoreCreateBinary();
-	xSemaphoreGive(i2c_semaphore);
-
-	return true;
-}
-
-bool i2c_stop() {
-	ESP_LOGD(TAG, "Stopping the I2C port driver");
-	esp_err_t delete_ret = i2c_driver_delete(I2C_PORT);
-	if (delete_ret != ESP_OK) {
-		ESP_LOGE(TAG, "Unable to delete the I2C driver (%i)", delete_ret);
-		return false;
-	}
-
-	// Delete the i2c semaphore
-	vSemaphoreDelete(i2c_semaphore);
-
-	return true;
-}
-
-bool i2c_read(const char address, const char reg, void* rd,
-		const size_t size, const time_t wait_ms) {
+static esp_err_t i2c_master_command(char addr, char reg, void *buf, size_t size,
+									time_t wait_ms, const uint8_t READ_BIT)
+{
 	if (size == 0)
-		return true;
+		return ESP_OK;
 
-	// Lock the thread
-	xSemaphoreTake(i2c_semaphore, portMAX_DELAY);
-
-	// Create a command handle
+	// create the command handle
 	const i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	bool ret = false;
 
-	do {
-		// Start the i2c master
-		ESP_LOGV(TAG, "Writing start condition");
-		if (i2c_master_start(cmd) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write stop condition");
-			break;
-		}
+	// queue the start, address, and register
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, reg, true);
 
-		// Write device address
-		ESP_LOGV(TAG, "Writing device address");
-		if (i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE,
-				true) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write device address");
-			break;
-		}
+	// queue the message payload
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (addr << 1) | READ_BIT, true);
+	if (size > 1)
+		i2c_master_read(cmd, buf, size - 1, I2C_MASTER_ACK);
+	i2c_master_read_byte(cmd, buf + size - 1, I2C_MASTER_NACK);
+	i2c_master_stop(cmd);
 
-		// Write register
-		ESP_LOGV(TAG, "Writing device register");
-		if (i2c_master_write_byte(cmd, reg, true) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write device register");
-			break;
-		}
+	// Get the amount of ticks to wait
+	const TickType_t ticks = wait_ms == 0 ? portMAX_DELAY : wait_ms / portTICK_PERIOD_MS;
 
-		// Send repeated start
-		ESP_LOGV(TAG, "Writing repeated start");
-		if (i2c_master_start(cmd) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to send repeated start");
-			break;
-		}
-
-		// Send device address and read data
-		ESP_LOGV(TAG, "Writing device address");
-		if (i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_READ,
-				true) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to send device address");
-			break;
-		}
-
-		// Read the data and the terminator
-		ESP_LOGV(TAG, "Writing read command");
-		if (i2c_master_read(cmd, (uint8_t*) rd, size, I2C_MASTER_ACK) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write read command");
-			break;
-		}
-		if (i2c_master_read_byte(cmd, (uint8_t*)(rd) + size, I2C_MASTER_NACK) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write read terminator command");
-			break;
-		}
-
-		// Stop the i2c master
-		ESP_LOGV(TAG, "Writing stop condition");
-		if (i2c_master_stop(cmd) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write stop condition");
-			break;
-		}
-
-		// Get the amount of ticks to wait
-		const TickType_t ticks = wait_ms == 0 ? portMAX_DELAY :
-				wait_ms / portTICK_PERIOD_MS;
-
-		// Send the data to the slave device
-		ESP_LOGV(TAG, "Sending the data in the queue");
-		esp_err_t send_ret = i2c_master_cmd_begin(I2C_PORT, cmd, ticks);
-		if (send_ret == ESP_ERR_INVALID_ARG)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (parameter error)");
-		else if (send_ret == ESP_FAIL)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (no response)");
-		else if (send_ret == ESP_ERR_INVALID_STATE)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (not installed)");
-		else if (send_ret == ESP_ERR_TIMEOUT)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (timed out)");
-		else {
-			// Log results
-			/*
-			char hex_str[size * 5];
-			strnhex(hex_str, reinterpret_cast<char*>(rd), size);
-			ESP_LOGD(TAG, "Got data from I2C 0x%02X, address 0x%02X: %s",
-					address, reg, hex_str);
-			*/
-			ret = true;
-		}
-	} while (false);
-
-	// Unlock the thread
-	xSemaphoreGive(i2c_semaphore);
-
-	// Delete the i2c command and return results
+	esp_err_t err = i2c_master_cmd_begin(I2C_PORT, cmd, ticks);
 	i2c_cmd_link_delete(cmd);
-	return ret;
+	return err;
 }
 
-bool i2c_write(const char address, const char reg, const void* wr,
-		const size_t size, const time_t wait_ms) {
-	if (size == 0)
-		return true;
+esp_err_t i2c_start()
+{
+	const i2c_config_t i2c_config = {
+		.mode = I2C_MODE_MASTER,		// set to master mode
+		.master = {
+			.clk_speed = 100000			// standard i2c speed
+		},
+		.sda_io_num = PIN_NUM_SDA,
+		.scl_io_num = PIN_NUM_SCL,
+		.sda_pullup_en = false,			// enable built-in pullup
+		.scl_pullup_en = false,			// enable built-in pullup
+	};
+	esp_err_t err = i2c_param_config(I2C_PORT, &i2c_config);
+	if (err)
+		return err;
+	err = i2c_driver_install(I2C_PORT, i2c_config.mode, 0, 0, 0);
+	return err;
+}
 
-	// Log write buffer as hex string
-	/*
-	char hex_str[size * 5];
-	strnhex(hex_str, reinterpret_cast<const char*>(wr), size);
-	ESP_LOGD(TAG, "Writing data to I2C 0x%02X, address 0x%02X: %s",
-			address, reg, hex_str);
-	*/
+esp_err_t i2c_stop()
+{
+	return i2c_driver_delete(I2C_PORT);
+}
 
-	// Create a command handle
-	const i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-	bool ret = false;
+esp_err_t i2c_read(char addr, char reg, void *buf, size_t size, time_t wait_ms)
+{
+	return i2c_master_command(addr, reg, buf, size, wait_ms, 1);
+}
 
-	do {
-		ESP_LOGV(TAG, "Writing start condition");
-		if (i2c_master_start(cmd) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write start condition");
-			break;
-		}
-
-		// Send device address (indicating write) and register to be written
-		ESP_LOGV(TAG, "Writing device address");
-		if (i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE,
-				true) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write device address");
-			break;
-		}
-
-		// Send register we want to write to
-		ESP_LOGV(TAG, "Writing device register");
-		if (i2c_master_write_byte(cmd, reg, 0x1) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write device register");
-			break;
-		}
-
-		// Put the data to be written in the i2c queue
-		ESP_LOGV(TAG, "Writing data");
-		if (i2c_master_write(cmd, (uint8_t*) wr, size, 0x1) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write data");
-			break;
-		}
-
-		// Stop the i2c master
-		ESP_LOGV(TAG, "Writing stop condition");
-		if (i2c_master_stop(cmd) != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to write stop condition");
-			break;
-		}
-
-		// Get the amount of ticks to wait
-		const TickType_t ticks = wait_ms == 0 ? portMAX_DELAY :
-				wait_ms / portTICK_PERIOD_MS;
-
-		// Send the data that is in the queue
-		ESP_LOGV(TAG, "Sending data in the queue");
-		esp_err_t send_ret = i2c_master_cmd_begin(I2C_PORT, cmd, ticks);
-		if (send_ret == ESP_ERR_INVALID_ARG)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (parameter error)");
-		else if (send_ret == ESP_FAIL)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (no response)");
-		else if (send_ret == ESP_ERR_INVALID_STATE)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (not installed)");
-		else if (send_ret == ESP_ERR_TIMEOUT)
-			ESP_LOGE(TAG, "Unable to write to the I2C port (timed out)");
-		else
-			ret = true;
-	} while (false);
-
-	// Delete the i2c command and return results
-	i2c_cmd_link_delete(cmd);
-	return ret;
+esp_err_t i2c_write(char addr, char reg, void *buf, size_t size, time_t wait_ms)
+{
+	return i2c_master_command(addr, reg, buf, size, wait_ms, 0);
 }
--- main/serial/include/i2c.h
+++ main/serial/include/i2c.h
@@ -1,55 +1,18 @@
 #ifndef MAIN_SERIAL_I2C_H_
 #define MAIN_SERIAL_I2C_H_
 
-#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#define LOG_LOCAL_LEVEL ESP_LOG_WARN
 
 #include "esp_system.h"
 #include "esp_log.h"
 #include "driver/i2c.h"
 
-#define I2C_PORT 	I2C_NUM_1
-#define PIN_NUM_SDA 23 // Adafruit Feather 32 Default
-#define PIN_NUM_SCL 22 // Adafruit Feather 32 Default
+esp_err_t i2c_start();
 
-/**
- * Start the I2C bus.
- *
- * @return true on success
- */
-bool i2c_start();
+esp_err_t i2c_stop();
 
-/**
- * Stops the I2C bus and frees memory.
- *
- * @return true on success
- */
-bool i2c_stop();
+esp_err_t i2c_read(char addr, char reg, void *buf, size_t size, time_t wait_ms);
 
-/**
- * Read from the I2C bus.
- *
- * @param address		the address of the desired I2C device
- * @param reg			the register to be read from the I2C device
- * @param rd			a pointer to the location to store the data that is read
- * @param size			the size in bytes to read
- * @param wait_ms		the amount of time in milliseconds to wait before timing out
- *
- * @return true on read success
- */
-bool i2c_read(const char address, const char reg, void* rd,
-		const size_t size, const time_t wait_ms);
-
-/**
- * Write to the I2C bus.
- *
- * @param address		the address of the desired I2C device
- * @param reg			the register to be written from the I2C device
- * @param wr			a pointer to the location of the data to write
- * @param size			the size in bytes to write
- * @param wait_ms		the amount of time in milliseconds to wait before timing out
- */
-bool i2c_write(const char address, const char reg, const void* wr,
-		const size_t size, const time_t wait_ms);
-
+esp_err_t i2c_write(char addr, char reg, void *buf, size_t size, time_t wait_ms);
 
 #endif /* MAIN_SERIAL_I2C_H_ */
