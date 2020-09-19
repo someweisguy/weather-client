17204d13be004bb900707a81f4d4715518aa44ca
Mitch Weisbrod <mitch@theweisbrods.com>
2020-09-19 11:17:04 -0700

    add i2s stubs

---
+++ components/serial/i2s.c
@@ -0,0 +1,11 @@
+#include "i2s.h"
+
+esp_err_t i2s_start()
+{
+    return ESP_FAIL;
+}
+
+esp_err_t i2s_stop()
+{
+    return ESP_FAIL;
+}
---
+++ components/serial/include/i2s.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "esp_system.h"
+
+esp_err_t i2s_start();
+
+esp_err_t i2s_stop();
