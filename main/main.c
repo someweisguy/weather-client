1a6bac4b5e155d8fad72e4810d3e0146a3c44805
Mitch Weisbrod <mitch@theweisbrods.com>
2020-09-19 11:17:32 -0700

    add i2s

--- main/main.c
+++ main/main.c
@@ -9,6 +9,8 @@
 
 #include "i2c.h"
 #include "uart.h"
+#include "i2s.h"
+
 #include "wlan.h"
 
 #include "driver/gpio.h"
@@ -17,18 +19,21 @@ static const char *TAG = "main";
 
 void app_main(void)
 {
-
     // create default event loop
     esp_event_loop_create_default();
 
     // start serial communications
     i2c_start();
     uart_start();
+    i2s_start();
 
     // init sensors
+    pms5003_reset();
+    max17043_reset();
+    bme280_reset();
+    const bme280_config_t bme_config = BME280_WEATHER_MONITORING;
+    bme280_set_config(&bme_config);
     
-    
-
     // init non-volatile storage
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
@@ -41,23 +46,6 @@ void app_main(void)
 
     wifi_start();
 
-    gpio_reset_pin(GPIO_NUM_13);
-    gpio_set_direction(GPIO_NUM_13, GPIO_MODE_INPUT_OUTPUT);
-
-    pms5003_reset();
-    pms5003_set_power(1);
-
-    max17043_reset();
-    max17043_config_t max_config;
-    max17043_get_config(&max_config);
-    printf("max17043 config: %04x, mode: %04x\n", max_config.config.val, 
-        max_config.mode);
-    
-    bme280_reset();
-    bme280_config_t bme_config = BME280_WEATHER_MONITORING;
-    bme280_set_config(&bme_config);
-  
-
     while (1)
     {
         bme280_data_t bme_data;
@@ -67,19 +55,14 @@ void app_main(void)
                bme_data.humidity, bme_data.pressure);
 
         pms5003_data_t pms_data;
-        esp_err_t pms_err = pms5003_get_data(&pms_data);
-        printf("PM2.5 at %d (checksum ", pms_data.concAtm.pm2_5);
-        if (pms_data.checksum_ok)
-            printf("OK)\n");
-        else
-            printf("FAIL)\n");
+        pms5003_get_data(&pms_data);
+        printf("PM2.5 at %d (checksum %s)\n", pms_data.concAtm.pm2_5, 
+            pms_data.checksum_ok ? "OK" : "FAIL");
 
         max17043_data_t max_data;
         max17043_get_data(&max_data);
         printf("Battery at %.2f%% (%.1fmV)\n", max_data.battery_life, 
             max_data.millivolts);
-
-        gpio_set_level(GPIO_NUM_13, !gpio_get_level(GPIO_NUM_13));
 
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
