333a67b5e317076be027c680c31bcc9ac87249e8
Mitch Weisbrod <mitch@theweisbrods.com>
2020-09-20 22:29:57 -0700

    increase sample rate

--- components/serial/i2s.c
+++ components/serial/i2s.c
@@ -13,11 +13,11 @@ esp_err_t i2s_init(void)
     // install i2s driver
     const i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_RX,
-        .sample_rate = 16000,
+        .sample_rate = 44100,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
         .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
-        .intr_alloc_flags = 0, // default interrupt priority
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 8,
         .dma_buf_len = 64,
         .use_apll = false
@@ -36,10 +36,6 @@ esp_err_t i2s_init(void)
     err = i2s_set_pin(I2S_NUM, &pin_config);
     if (err)   
         ESP_LOGE("i2s", "i2s set pin error %x", err);
-
-    err = i2s_zero_dma_buffer(I2S_NUM);
-    if (err)
-        ESP_LOGE("i2s", "i2s zero dma error %x", err);
 
     return ESP_OK;
 }
