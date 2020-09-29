abd4022208e5df4b4c9832f8048ae498733ad93c
Mitch Weisbrod <mitch@theweisbrods.com>
2020-09-29 09:16:57 -0700

    use default i2s read method

--- components/serial/include/i2s.h
+++ components/serial/include/i2s.h
@@ -1,9 +1,8 @@
 #pragma once
 
 #include "esp_system.h"
+#include "driver/i2s.h"
 
 esp_err_t i2s_init(void);
 
 esp_err_t i2s_end(void);
-
-esp_err_t i2s_master_read(void *buf, size_t size, time_t wait_ms);
