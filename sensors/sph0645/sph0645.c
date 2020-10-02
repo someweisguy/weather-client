1ef3c7313d9cd83e043b59bd7211944bfa4d7fe8
Mitch Weisbrod <mitch@theweisbrods.com>
2020-10-01 22:52:56 -0700

    implement methods

--- sensors/sph0645/sph0645.c
+++ sensors/sph0645/sph0645.c
@@ -8,182 +8,178 @@
 #include "i2s.h"
 
 #include <math.h>
+#include <string.h>
+
+#define SAMPLE_RATE 48000 // Hz, fixed to design of IIR filters
+#define SAMPLE_BITS 32    // Number of bits received in the i2s frame.
+
+#define MIC_SENSITIVITY -26   // dBFS value expected at MIC_REF_DB (value from datasheet)
+#define MIC_REF_DB 94.0       // Value at which point sensitivity is specified in datasheet (dB)
+#define MIC_OVERLOAD_DB 116.0 // Acoustic overload point of the microphone (dB).
+#define MIC_NOISE_DB 29       // Noise floor of the microphone (dB).
+#define MIC_BITS 24           // Valid number of bits in i2s frame. Must be less than or equal to SAMPLE_BITS.
+#define MIC_POWER_UP_TIME 50  // Power-up time of the microphone (ms).
+#define MIC_OFFSET_DB 3.0103  // Default offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration.
 
 #define MIN(a, b) ((a < b) ? a : b)
 #define MAX(a, b) ((a > b) ? a : b)
 
-// value of 420426 == 94dBa @ 10kHz
-// see https://hackaday.io/project/166867-esp32-i2s-slm
-// https://github.com/ikostoski/esp32-i2s-slm/blob/master/esp32-i2s-slm.ino
+static TaskHandle_t mic_reader_task_handle = NULL;
+static float samples[6000];
+//static float *samples = NULL;
+static sph0645_data_t task_data;
+static sph0645_config_t task_config;
 
-/*
-n = readout from microphone
-20 * log(n / 2**23-1) == dBFS
-dBFS + 94 == dB
-*/
-
-#define LEQ_PERIOD 1 // second(s)
-
-#define SAMPLE_RATE 48000 // Hz, fixed to design of IIR filters
-#define SAMPLE_BITS 32    // bits
-#define SAMPLE_T int32_t
-#define SAMPLES_SHORT (SAMPLE_RATE / 8) // ~125ms
-#define SAMPLES_LEQ (SAMPLE_RATE * LEQ_PERIOD)
-#define DMA_BANK_SIZE (SAMPLES_SHORT / 16)
-#define DMA_BANKS 32
-
-#define I2S_TASK_PRI 4
-#define I2S_TASK_STACK 2048
-
-#define MIC_SENSITIVITY -26   // dBFS value expected at MIC_REF_DB (Sensitivity value from datasheet)
-#define MIC_REF_DB 94.0       // Value at which point sensitivity is specified in datasheet (dB)
-#define MIC_OVERLOAD_DB 116.0 // dB - Acoustic overload point
-#define MIC_NOISE_DB 29       // dB - Noise floor
-#define MIC_BITS 24           // valid number of bits in I2S data
-#define MIC_CONVERT(s)    (s >> (SAMPLE_BITS - MIC_BITS))
-
-#define MIC_OFFSET_DB -2//3.0103 // Default offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration
-
-static const double MIC_REF_AMPL = 420426.27363163716; // pow(10, (double)MIC_SENSITIVITY / 20) * ((1 << (MIC_BITS - 1)) - 1);
-
-// Data we push to 'samples_queue'
-typedef struct
+static void mic_reader_task(void *arg)
 {
-    float sum_sqr_SPL;      // Sum of squares of mic samples, after Equalizer filter
-    float sum_sqr_weighted; // Sum of squares of weighted mic samples
-    uint32_t proc_ticks;    // Debug only, FreeRTOS ticks we spent processing the I2S data
-} sum_queue_t;
+    const size_t num_samples = SAMPLE_RATE / 1000 * task_config.sample_length;
+    const double mic_ref_ampl = pow10(MIC_SENSITIVITY / 20.0) * ((1 << (MIC_BITS - 1)) - 1);
 
-static float samples[SAMPLES_SHORT] = {};
-static QueueHandle_t samples_queue;
-
-static void mic_i2s_reader_task(void *parameter)
-{
-    // Discard first block, microphone may have startup time (i.e. INMP441 up to 83ms)
+    uint64_t acc_samples = 0;
+    double acc_sum_sqr = 0;
     size_t bytes_read = 0;
-    i2s_read(0, &samples, SAMPLES_SHORT * sizeof(int32_t), &bytes_read, portMAX_DELAY);
+
+    // Reset the running data
+    task_data.avg = 0;
+    task_data.samples = 0;
+    task_data.max = -INFINITY;
+    task_data.min = INFINITY;
 
     while (true)
     {
-        // Block and wait for microphone values from I2S
-        //
-        // Data is moved from DMA buffers to our 'samples' buffer by the driver ISR
-        // and when there is requested ammount of data, task is unblocked
-        //
-        // Note: i2s_read does not care it is writing in float[] buffer, it will write
-        //       integer values to the given address, as received from the hardware peripheral.
-        i2s_read(0, &samples, SAMPLES_SHORT * sizeof(SAMPLE_T), &bytes_read, portMAX_DELAY);
+        // Block and wait for microphone values from i2s
+        i2s_read(0, samples, num_samples * sizeof(int32_t), &bytes_read, portMAX_DELAY);
 
-        TickType_t start_tick = xTaskGetTickCount();
+        // Convert integer microphone values to floats
+        int32_t *int_samples = (int32_t *)&samples;
+        for (int i = 0; i < num_samples; i++)
+            samples[i] = int_samples[i] >> (SAMPLE_BITS - MIC_BITS);
 
-        // Convert (including shifting) integer microphone values to floats,
-        // using the same buffer (assumed sample size is same as size of float),
-        // to save a bit of memory
-        SAMPLE_T *int_samples = (SAMPLE_T *)&samples;
-        for (int i = 0; i < SAMPLES_SHORT; i++)
-            samples[i] = MIC_CONVERT(int_samples[i]);
+        // Apply equalization and get C-weighted sum of squares
+        const float sum_sqr_z = equalize(samples, samples, num_samples);
+        const float sum_sqr_c = weight_dBC(samples, samples, num_samples);
 
-        sum_queue_t q;
-        // Apply equalization and calculate Z-weighted sum of squares,
-        // writes filtered samples back to the same buffer.
-        q.sum_sqr_SPL = equalize(samples, samples, SAMPLES_SHORT);
-        //Serial.printf("Val: %f\n", q.sum_sqr_SPL);
+        // Calculate dB values relative to mic_ref_ampl and adjust for microphone reference
+        const double rms_z = sqrt((double)sum_sqr_z / num_samples);
+        const double dBz = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(rms_z / mic_ref_ampl);
 
-        // Apply weighting and calucate weigthed sum of squares
-        q.sum_sqr_weighted = weight_dBC(samples, samples, SAMPLES_SHORT);
+        // In case of acoustic overload or below noise floor measurement, report infinty
+        if (dBz > MIC_OVERLOAD_DB)
+            acc_sum_sqr = INFINITY;
+        else if (isnan(dBz) || (dBz < MIC_NOISE_DB))
+            acc_sum_sqr = NAN;
 
-        // Debug only. Ticks we spent filtering and summing block of I2S data
-        q.proc_ticks = xTaskGetTickCount() - start_tick;
+        // Accumulate the C-weighted sum of squares
+        acc_sum_sqr += sum_sqr_c;
+        acc_samples += num_samples;
 
-        // Send the sums to FreeRTOS queue where main task will pick them up
-        // and further calcualte decibel values (division, logarithms, etc...)
-        xQueueSend(samples_queue, &q, portMAX_DELAY);
+        // When we gather enough samples, calculate the RMS C-weighted value
+        if (acc_samples >= SAMPLE_RATE * task_config.sample_period)
+        {
+            vTaskSuspendAll(); // enter critical section, interrupts enabled
+
+            const double rms_c = sqrt(acc_sum_sqr / acc_samples);
+            const double dBc = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(rms_c / mic_ref_ampl);
+
+            // Add the data to the currently running data
+            task_data.avg += dBc;
+            task_data.min = MIN(task_data.min, dBc);
+            task_data.max = MAX(task_data.max, dBc);
+            ++task_data.samples;
+
+            xTaskResumeAll(); // exit critical section
+
+            // zero out the accumulators
+            acc_sum_sqr = 0;
+            acc_samples = 0;
+        }
     }
 }
 
-esp_err_t stub()
+esp_err_t sph0645_reset()
 {
-    samples_queue = xQueueCreate(8, sizeof(sum_queue_t));
-    xTaskCreate(mic_i2s_reader_task, "Mic I2S Reader", I2S_TASK_STACK, NULL, I2S_TASK_PRI, NULL);
-
-    sum_queue_t q;
-    uint32_t Leq_samples = 0;
-    double Leq_sum_sqr = 0;
-    double Leq_dB = 0;
-
-    // Read sum of samaples, calculated by 'i2s_reader_task'
-    while (xQueueReceive(samples_queue, &q, portMAX_DELAY))
+    if (samples == NULL)
     {
-
-        // Calculate dB values relative to MIC_REF_AMPL and adjust for microphone reference
-        double short_RMS = sqrt(((double)q.sum_sqr_SPL) / SAMPLES_SHORT);
-        double short_SPL_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(short_RMS / MIC_REF_AMPL);
-
-        // In case of acoustic overload or below noise floor measurement, report infinty Leq value
-        if (short_SPL_dB > MIC_OVERLOAD_DB)
+        // Discard data to allow for mic startup
+        const size_t num_samples = SAMPLE_RATE / 1000 * MIC_POWER_UP_TIME;
+        for (int i = 0; i < num_samples; ++i)
         {
-            Leq_sum_sqr = INFINITY;
+            size_t bytes_read;
+            int32_t discard;
+            esp_err_t err = i2s_read(0, &discard, sizeof(int32_t), &bytes_read, 1);
+            if (err)
+                return err;
         }
-        else if (isnan(short_SPL_dB) || (short_SPL_dB < MIC_NOISE_DB))
-        {
-            Leq_sum_sqr = -INFINITY;
-        }
-
-        // Accumulate Leq sum
-        Leq_sum_sqr += q.sum_sqr_weighted;
-        Leq_samples += SAMPLES_SHORT;
-
-        // When we gather enough samples, calculate new Leq value
-        if (Leq_samples >= SAMPLE_RATE * LEQ_PERIOD)
-        {
-            double Leq_RMS = sqrt(Leq_sum_sqr / Leq_samples);
-            Leq_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(Leq_RMS / MIC_REF_AMPL);
-            Leq_sum_sqr = 0;
-            Leq_samples = 0;
-
-            // Serial output, customize (or remove) as needed
-            printf("%.1f\n", Leq_dB);
-
-            // Debug only
-            //printf("%u processing ticks\n", q.proc_ticks);
-        }
-
-#if (USE_DISPLAY > 0)
-
-        //
-        // Example code that displays the measured value.
-        // You should customize the below code for your display
-        // and display library used.
-        //
-
-        display.clear();
-
-        // It is important to somehow notify when the deivce is out of its range
-        // as the calculated values are very likely with big error
-        if (Leq_dB > MIC_OVERLOAD_DB)
-        {
-            // Display 'Overload' if dB value is over the AOP
-            display.drawString(0, 24, "Overload");
-        }
-        else if (isnan(Leq_dB) || (Leq_dB < MIC_NOISE_DB))
-        {
-            // Display 'Low' if dB value is below noise floor
-            display.drawString(0, 24, "Low");
-        }
-
-        // The 'short' Leq line
-        double short_Leq_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(sqrt(double(q.sum_sqr_weighted) / SAMPLES_SHORT) / MIC_REF_AMPL);
-        uint16_t len = min(max(0, int(((short_Leq_dB - MIC_NOISE_DB) / MIC_OVERLOAD_DB) * (display.getWidth() - 1))), display.getWidth() - 1);
-        display.drawHorizontalLine(0, 0, len);
-        display.drawHorizontalLine(0, 1, len);
-        display.drawHorizontalLine(0, 2, len);
-
-        // The Leq numeric decibels
-        display.drawString(0, 4, String(Leq_dB, 1) + " " + DB_UNITS);
-
-        display.display();
-
-#endif // USE_DISPLAY
     }
+    else
+    {
+        // Delete the task if it is currently running
+        if (mic_reader_task_handle != NULL)
+            vTaskDelete(mic_reader_task_handle);
+
+        // Create the reader task
+        xTaskCreate(mic_reader_task, "i2s_mic_reader", 2048, NULL, 4,
+                    &mic_reader_task_handle);
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t sph0645_set_config(const sph0645_config_t *config)
+{
+    if (config->sample_length == 0 || config->sample_period == 0)
+        return ESP_ERR_INVALID_ARG;
+
+    // Suspend the currently running mic task
+    if (mic_reader_task_handle != NULL)
+        vTaskSuspend(mic_reader_task_handle);
+
+    // // Check to see if we can malloc a large enough block of memory for samples
+    // const size_t num_samples = SAMPLE_RATE / 1000 * config->sample_length;
+    // float *tmp_samples = realloc(samples, num_samples * sizeof(float));
+    // if (tmp_samples == NULL)
+    // {
+    //     if (mic_reader_task_handle != NULL)
+    //         vTaskResume(mic_reader_task_handle);
+    //     return ESP_ERR_NO_MEM;
+    // }
+    // samples = tmp_samples;
+
+    // Copy argument to task_config
+    memcpy(&task_config, config, sizeof(task_config));
+
+    // Restart the task automatically if it was running
+    if (mic_reader_task_handle != NULL)
+        vTaskDelete(mic_reader_task_handle);
+    xTaskCreate(mic_reader_task, "i2s_mic_reader", 2048, NULL, 4, &mic_reader_task_handle);
+
+    return ESP_OK;
+}
+
+esp_err_t sph0645_get_config(sph0645_config_t *config)
+{
+    memcpy(config, &task_config, sizeof(task_config));
+    return ESP_OK;
+}
+
+esp_err_t sph0645_get_data(sph0645_data_t *data)
+{
+    if (mic_reader_task_handle == NULL)
+        return ESP_ERR_INVALID_STATE;
+
+    vTaskSuspendAll(); // enter critical section, interrupts enabled
+
+    // Get the average and copy the data over
+    task_data.avg /= task_data.samples;
+    memcpy(data, &task_data, sizeof(task_data));
+
+    // Reset the task data
+    task_data.avg = 0;
+    task_data.samples = 0;
+    task_data.max = -INFINITY;
+    task_data.min = INFINITY;
+
+    xTaskResumeAll(); // exit critical section
+
     return ESP_OK;
 }
--- sensors/sph0645/sph0645.h
+++ sensors/sph0645/sph0645.h
@@ -3,7 +3,22 @@
 #include "esp_system.h"
 
 typedef struct {
+    float avg;
+    float min;
+    float max;
+    uint64_t samples;
+} sph0645_data_t;
 
+typedef struct {
+    uint32_t sample_length;         // Length of time in which audio samples are taken (ms).
+    uint32_t sample_period;         // Period in which audio values are calculated (seconds).
 } sph0645_config_t;
 
-esp_err_t stub();
+#define SPH0645_DEFAULT_CONFIG { .sample_length = 125, .sample_period = 1}
+
+esp_err_t sph0645_reset();
+
+esp_err_t sph0645_set_config(const sph0645_config_t *config);
+esp_err_t sph0645_get_config(sph0645_config_t *config);
+
+esp_err_t sph0645_get_data(sph0645_data_t *data);
