7e2ce114c40e246899987557403c40717abd5269
Mitch Weisbrod <mitch@theweisbrods.com>
2020-10-04 21:29:26 -0700

    add sensor root node

--- main/http_handlers/http_about_handler.h
+++ main/http_handlers/http_about_handler.h
@@ -20,6 +20,7 @@ esp_err_t http_about_handler(httpd_req_t *r)
 
     cJSON* root = cJSON_CreateObject();
 
+    // add the compilation timestamp
     cJSON_AddStringToObject(root, "compiled", __DATE__ " " __TIME__);
 
     // get the system up time
@@ -28,22 +29,29 @@ esp_err_t http_about_handler(httpd_req_t *r)
     const uint64_t system_up_time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
     cJSON_AddNumberToObject(root, "up_time", system_up_time);
 
+    // create a root for the sensors
+    cJSON *sensor_root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "sensors", sensor_root);
 
+    // report max17043 status
     max17043_config_t max_config;
     err = max17043_get_config(&max_config);
-    cJSON_AddStringToObject(root, "max17043", esp_err_to_name(err));
+    cJSON_AddStringToObject(sensor_root, "max17043", esp_err_to_name(err));
 
+    // report bme280 status
     bme280_config_t bme_config;
     err = bme280_get_config(&bme_config);
-    cJSON_AddStringToObject(root, "bme280", esp_err_to_name(err));
+    cJSON_AddStringToObject(sensor_root, "bme280", esp_err_to_name(err));
 
+    // reposrt pms5003 status
     pms5003_config_t pms_config;
     err = pms5003_get_config(&pms_config);
-    cJSON_AddStringToObject(root, "pms5003", esp_err_to_name(err));
+    cJSON_AddStringToObject(sensor_root, "pms5003", esp_err_to_name(err));
 
+    // report sph0645 status
     sph0645_config_t sph_config;
     err = sph0645_get_config(&sph_config);
-    cJSON_AddStringToObject(root, "sph0645", esp_err_to_name(err));
+    cJSON_AddStringToObject(sensor_root, "sph0645", esp_err_to_name(err));
 
     // render the json as a string
     char *rendered = cJSON_Print(root);
