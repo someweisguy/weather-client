cd7cac388dc3eb025605913b217173144b994613
Mitch Weisbrod <mitch@theweisbrods.com>
2020-10-06 19:43:22 -0700

    use null terminated topic string

--- components/network/mqtt.c
+++ components/network/mqtt.c
@@ -50,7 +50,7 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
                     .topic = subscriptions[i].topic};
                 esp_err_t err = subscriptions[i].cb(&r);
                 if (err)
-                    ESP_LOGW(TAG, "function callback on topic '%s' returned error", event->topic);
+                    ESP_LOGW(TAG, "function callback on topic '%s' returned error", topic);
                 return err;
             }
         }
