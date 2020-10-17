8ed38b1c6f8ce723b4e038203bc45edc2952b350
Mitch Weisbrod <mitch@theweisbrods.com>
2020-10-16 23:22:38 -0700

    mqtt responds with json only

--- main/client_handlers/mqtt_handlers.c
+++ main/client_handlers/mqtt_handlers.c
@@ -1,11 +1,13 @@
 #include "mqtt_handlers.h"
 
+#include <string.h>
 #include "client_handlers/client_handlers.h"
 
 #include "cJSON.h"
 
 #define DEFAULT_QOS 1
-#define JSON_RESP(k, v) ("{\n\t\"" k "\":\t\"" v "\"\n}")
+#define RESP_STR_LENGTH 64
+#define RESP_FORMAT "{\n\t\"%s\":\t\"%s\"\n}"
 
 static const char* MQTT_RESP_SUFFIX = "/res";
 
@@ -27,6 +29,8 @@ esp_err_t mqtt_about_handler(mqtt_req_t *r)
 
 esp_err_t mqtt_config_handler(mqtt_req_t *r)
 {
+    char resp_str[RESP_STR_LENGTH];
+
     // build the response topic
     char resp_topic[strlen(MQTT_RESP_SUFFIX) + strlen(r->topic) + 1];
     strcpy(resp_topic, r->topic);
@@ -35,15 +39,20 @@ esp_err_t mqtt_config_handler(mqtt_req_t *r)
     // read the body of the request into memory
     char *request = malloc(r->content_len + 1); // content + null terminator
     if (request == NULL)
-    {
-        mqtt_resp_sendstr(r, resp_topic, JSON_RESP("error", "NO MEMORY"), DEFAULT_QOS, false);
+    {   
+        snprintf(resp_str, sizeof(resp_str), RESP_FORMAT, "error", esp_err_to_name(ESP_ERR_NO_MEM));
+        mqtt_resp_sendstr(r, resp_topic, resp_str, DEFAULT_QOS, false);
         return ESP_ERR_NO_MEM;
     }
     memcpy(request, r->content, r->content_len);
 
     // send a response to the client
+    char *resp_key = "response";
     esp_err_t err = config_handler(request);
-    mqtt_resp_sendstr(r, resp_topic, esp_err_to_name(err), DEFAULT_QOS, false);
+    if (err)
+        resp_key = "error";
+    snprintf(resp_str, sizeof(resp_str), RESP_FORMAT, resp_key, esp_err_to_name(err));
+    mqtt_resp_sendstr(r, resp_topic, resp_str, DEFAULT_QOS, false);
 
     free(request);
 
@@ -52,6 +61,8 @@ esp_err_t mqtt_config_handler(mqtt_req_t *r)
 
 esp_err_t mqtt_data_handler(mqtt_req_t *r)
 {
+    char resp_str[RESP_STR_LENGTH];
+
     // build the response topic
     char resp_topic[strlen(MQTT_RESP_SUFFIX) + strlen(r->topic) + 1];
     strcpy(resp_topic, r->topic);
@@ -61,7 +72,8 @@ esp_err_t mqtt_data_handler(mqtt_req_t *r)
     char *request = malloc(r->content_len + 1); // content + null terminator
     if (request == NULL)
     {
-        mqtt_resp_sendstr(r, resp_topic, JSON_RESP("error", "NO MEMORY"), DEFAULT_QOS, false);
+        snprintf(resp_str, sizeof(resp_str), RESP_FORMAT, "error", esp_err_to_name(ESP_ERR_NO_MEM));
+        mqtt_resp_sendstr(r, resp_topic, resp_str, DEFAULT_QOS, false);
         return ESP_ERR_NO_MEM;
     }
     memcpy(request, r->content, r->content_len);
@@ -86,12 +98,15 @@ esp_err_t mqtt_data_handler(mqtt_req_t *r)
 
 esp_err_t mqtt_restart_handler(mqtt_req_t *r)
 {
+    char resp_str[RESP_STR_LENGTH];
+
     // build the response topic
     char resp_topic[strlen(MQTT_RESP_SUFFIX) + strlen(r->topic) + 1];
     strcpy(resp_topic, r->topic);
     strcat(resp_topic, MQTT_RESP_SUFFIX);
 
-    mqtt_resp_sendstr(r, resp_topic, JSON_RESP("response", "OK"), DEFAULT_QOS, false);
+    snprintf(resp_str, sizeof(resp_str), RESP_FORMAT, "response", esp_err_to_name(ESP_OK));
+    mqtt_resp_sendstr(r, resp_topic, resp_str, DEFAULT_QOS, false);
     restart_handler();
 
     return ESP_OK;
