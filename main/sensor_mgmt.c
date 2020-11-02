72418a9445e7d21cf0d9d0cd1825aabcafdee9e6
Mitch Weisbrod <mitch@theweisbrods.com>
2020-11-01 22:46:52 -0800

    add json keys

--- main/sensor_mgmt.c
+++ main/sensor_mgmt.c
@@ -1,38 +1,41 @@
 #include "sensor_mgmt.h"
 
-#include "cJSON.h"
-
 #include "bme280.h"
+#include "cJSON.h"
 #include "max17043.h"
 #include "pms5003.h"
 #include "sph0645.h"
 #include "wireless.h"
 
-
-
 // wifi json keys
-#define JSON_RSSI_KEY           "signal_strength"
+#define JSON_RSSI_KEY "signal_strength"
 // max17043 json keys
-#define JSON_BATT_KEY           "battery"
+#define JSON_BATT_KEY "battery"
 // bme280 json keys
-#define JSON_TEMPERATURE_KEY    "temperature"
-#define JSON_HUMIDITY_KEY       "humidity"
-#define JSON_PRESSURE_KEY       "pressure"
-#define JSON_DEW_POINT_KEY      "dew_point"
-#define JSON_ELEVATION_KEY      "elevation"
+#define JSON_TEMPERATURE_KEY "temperature"
+#define JSON_HUMIDITY_KEY "humidity"
+#define JSON_PRESSURE_KEY "pressure"
+#define JSON_DEW_POINT_KEY "dew_point"
+#define JSON_ELEVATION_KEY "elevation"
 // pms5003 json keys
-#define JSON_PM1_KEY            "pm1"
-#define JSON_PM2_5_KEY          "pm2_5"
-#define JSON_PM10_KEY           "pm10"
-#define JSON_FAN_KEY            "fan"
+#define JSON_PM1_KEY "pm1"
+#define JSON_PM2_5_KEY "pm2_5"
+#define JSON_PM10_KEY "pm10"
+#define JSON_FAN_KEY "fan"
 // sph0645 json keys
-#define JSON_AVG_NOISE_KEY      "avg_noise"
-#define JSON_MIN_NOISE_KEY      "min_noise"
-#define JSON_MAX_NOISE_KEY      "max_noise"
+#define JSON_AVG_NOISE_KEY "avg_noise"
+#define JSON_MIN_NOISE_KEY "min_noise"
+#define JSON_MAX_NOISE_KEY "max_noise"
 
-#define TRUNCATE(n)             (((int64_t)(n * 100)) / 100)
+#define UNIQUE_ID(n) (MQTT_CLIENT_NAME "_" n)
+#define SENSOR_TOPIC(n) ("homeassistant/sensor/" MQTT_CLIENT_NAME "_" n "/config")
+#define BINARY_SENSOR_TOPIC(n) ("homeassistant/binary_sensor/" MQTT_CLIENT_NAME "_" n "/config")
+#define VALUE_TEMPLATE1(a) ("{{ value_json['" a "'] }}")
+#define VALUE_TEMPLATE2(a, b) ("{{ value_json['" a "']['" b "'] }}")
 
-void sensors_start(cJSON *json)
+#define TRUNCATE(n) (((int64_t)(n * 100)) / 100)
+
+void sensors_start()
 {
     esp_err_t err = ESP_OK;
 #ifdef USE_BME280
@@ -133,6 +136,13 @@ void sensors_get_data(cJSON *json)
     } while (false);
 #endif // USE BME280
 
+#ifdef USE_PMS5003
+
+#endif // USE_PMS5003
+
+#ifdef USE_SPH0645
+
+#endif // USE_PMS5003
 }
 
 void sensors_sleep(cJSON *json)
