d5ced8529a3081b6a3c496e4067c957a73602b25
Mitch Weisbrod <mitch@theweisbrods.com>
2021-04-23 13:02:26 -0700

    add pms5003 get_data

--- components/pms5003/pms5003.hpp
+++ components/pms5003/pms5003.hpp
@@ -34,7 +34,31 @@ private:
             .value_template = "{{ json." PM_10_KEY " }}"
           }
         }
-      };
+    };
+  
+  struct pms_data_t { 
+    uint16_t start_word;
+    uint16_t len;
+    struct {
+      uint16_t pm1;
+      uint16_t pm2_5;
+      uint16_t pm10;
+    } standard;
+    struct {
+      uint16_t pm1;
+      uint16_t pm2_5;
+      uint16_t pm10;
+    } atmospheric;
+    uint16_t num_particles_0_3um;
+    uint16_t num_particles_0_5um;
+    uint16_t num_particles_1um;
+    uint16_t num_particles_2_5um;
+    uint16_t num_particles_5um;
+    uint16_t num_particles_10um;
+    uint16_t : 16; // reserved
+    uint16_t checksum;
+  };
+
 
 public:
   pms5003_t() : Sensor("pms5003") {
@@ -75,7 +99,32 @@ public:
   }
 
   esp_err_t get_data(cJSON *json) {
-    // TODO
+    // force a measurement
+    const uint8_t read_cmd[] = {0x42, 0x4d, 0xe2, 0x00, 0x00, 0x01, 0x71};
+    esp_err_t err = serial_uart_write(read_cmd, 7, 100 / portTICK_PERIOD_MS);
+    if (err) return err;
+
+    // read the data from the measurement
+    pms_data_t data;
+    err = serial_uart_read(&data, 32, 1000 / portTICK_PERIOD_MS);
+    if (err) return err;
+
+    // swap data endianness
+    uint8_t *const raw_data = reinterpret_cast<uint8_t *>(&data);
+    for (int i = 3; i < 32; i += 2) {
+      const uint8_t temp = raw_data[i - 1];
+      raw_data[i - 1] = raw_data[i];
+      raw_data[i] = temp;
+    }
+
+    // verify checksum
+    uint16_t checksum = 0;
+    for (int i = 0; i < 30; ++i) checksum += raw_data[i];
+    if (checksum != data.checksum) return ESP_ERR_INVALID_CRC;
+
+    cJSON_AddNumberToObject(json, PM_2_5_KEY, data.atmospheric.pm2_5);
+    cJSON_AddNumberToObject(json, PM_10_KEY, data.atmospheric.pm10);
+
     return ESP_OK;
   }
 
